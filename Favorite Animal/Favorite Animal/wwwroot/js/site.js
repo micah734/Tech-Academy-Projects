7ac64bfda0f26e1d13972b8e29b9122d100755c8
Micah Gooden <micah734@me.com>
2022-02-14 21:29:29 -0500

    update

---
+++ Favorite Animal/Favorite Animal/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+var imgObj;
+var animate;
+function init() {
+    imgObj = document.getElementById('image');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 5 + 'px';
+    animate = setTimeout(moveRight, 20);
+}
+
+function stop() {
+    clearTimeout(animate) 
+    imgObj.style.left='0px'
+}
+
+window.onload = init;
